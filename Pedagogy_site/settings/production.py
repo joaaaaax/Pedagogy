a75280c86f7a401be1519377c20fe1cecfae2291
Sean <quiccryl@gmail.com>
2022-11-13 18:10:14 +0800

    t

--- Pedagogy_site/settings/production.py
+++ Pedagogy_site/settings/production.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [BASE_DIR / "static_my_project"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-from Pedagogy_site.aws.conf import *
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
