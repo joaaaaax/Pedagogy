e4de8c3a2c0fe98caa7f3b34699f235ea627b9ed
Sean <quiccryl@gmail.com>
2022-11-13 21:27:02 +0800

    revert to orig

--- Pedagogy_site/aws/conf.py
+++ Pedagogy_site/aws/conf.py
@@ -1,10 +1,12 @@
 import datetime
 
+AWS_GROUP_NAME = "CPE_User_Group"
+AWS_USERNAME = "waks"
 AWS_ACCESS_KEY_ID = "AKIA4U6BZYMTHYE4DZMJ"
 AWS_SECRET_ACCESS_KEY = "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"
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
-AWS_QUERYSTRING_AUTH = False
+AWS_QUERYSTRING_AUTH = True
 
 DEFAULT_FILE_STORAGE = 'Pedagogy_site.aws.utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'Pedagogy_site.aws.utils.StaticRootS3BotoStorage'
@@ -24,3 +26,5 @@ AWS_HEADERS = {
     'Expires': expires,
     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
 }
+
+AUTH_PASSWORD_VALIDATORS = True
