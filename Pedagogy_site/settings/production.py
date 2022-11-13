db51785420f7d36c285232c8a1af434bc257d164
Sean <quiccryl@gmail.com>
2022-11-13 18:44:45 +0800

    test

--- Pedagogy_site/settings/production.py
+++ Pedagogy_site/settings/production.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
-STATICFILES_DIRS = [BASE_DIR / "static_my_project"]
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
